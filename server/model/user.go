ed418d6ed8ebc75e9bf08b9bb18698a8458029e8
Alex <alexandre@yakow.io>
2022-05-26 21:32:15 +0200

    fix: don't serialize hashed password in json response

--- server/model/user.go
+++ server/model/user.go
@@ -1,10 +1,10 @@
 package model
 
 type User struct {
-	Firstname      string `json:"firstname" binding:"required,min=2,max=50"`
-	Lastname       string `json:"lastname" binding:"required,min=2,max=50"`
-	Email          string `json:"email" binding:"required,email"`
-	HashedPassword string
+	Firstname      string      `json:"firstname" binding:"required,min=2,max=50"`
+	Lastname       string      `json:"lastname" binding:"required,min=2,max=50"`
+	Email          string      `json:"email" binding:"required,email"`
+	HashedPassword string      `json:"-"`
 	Phone          string      `json:"phone" binding:"required,e164,len=12"`
 	Trips          []*RoadTrip `json:"roadTrip"`
 }
