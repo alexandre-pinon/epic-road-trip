aa4a205947f3fc16701817c1cf6b2ca93a624378
Alex <alexandre@yakow.io>
2022-05-29 16:04:55 +0200

    test: payload func

--- server/service/auth_service.go
+++ server/service/auth_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"github.com/alexandre-pinon/epic-road-trip/repository"
+	jwt "github.com/appleboy/gin-jwt/v2"
 )
 
 type authService struct {
@@ -9,8 +10,13 @@ type authService struct {
 }
 
 type AuthService interface {
+	PayloadFunc(data interface{}) jwt.MapClaims
 }
 
 func NewAuthService(repo repository.UserRepository) AuthService {
 	return &authService{repo}
+}
+
+func (svc *authService) PayloadFunc(data interface{}) jwt.MapClaims {
+	return nil
 }
--- server/service/auth_service_test.go
+++ server/service/auth_service_test.go
@@ -4,7 +4,10 @@ import (
 	"testing"
 
 	"github.com/alexandre-pinon/epic-road-trip/mocks"
+	"github.com/alexandre-pinon/epic-road-trip/model"
+	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type authServiceSuite struct {
@@ -19,6 +22,31 @@ func (suite *authServiceSuite) SetupTest() {
 
 	suite.repo = repo
 	suite.authService = authService
+}
+
+func (suite *authServiceSuite) TestPayloadFunc_Positive() {
+	ID := primitive.NewObjectID()
+	payload := &model.User{
+		ID:             ID,
+		Firstname:      "yoimiya",
+		Lastname:       "naganohara",
+		Email:          "yoimiya.naganohara@gmail.com",
+		HashedPassword: "12345678",
+		Phone:          "+33612345678",
+	}
+	claims := suite.authService.PayloadFunc(payload)
+
+	suite.NotNil(claims)
+	suite.NotEmpty(claims)
+	suite.Equal(ID, claims[jwt.IdentityKey])
+}
+
+func (suite *authServiceSuite) TestPayloadFunc_InvalidData_Positive() {
+	payload := struct{}{}
+	claims := suite.authService.PayloadFunc(payload)
+
+	suite.NotNil(claims)
+	suite.Empty(claims)
 }
 
 func TestAuthService(t *testing.T) {
