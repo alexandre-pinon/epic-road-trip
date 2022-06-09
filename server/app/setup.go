d7a324e922c5a54c5b4f74af01e689e5888f96e6
Alex <alexandre@yakow.io>
2022-06-09 12:25:36 +0200

    feat: rm go mode to launch server

--- server/app/setup.go
+++ server/app/setup.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"log"
-	"os"
 
 	"github.com/alexandre-pinon/epic-road-trip/config"
 	"github.com/alexandre-pinon/epic-road-trip/docs"
@@ -10,15 +9,9 @@ import (
 )
 
 func InitApp() {
-	env := os.Getenv("GO_MODE")
-
-	if err := config.Env(env).IsValid(); err != nil {
-		log.Fatal(err)
-	}
-
 	log.Print("Initializing app...")
 
-	cfg := config.GetConfig(config.Env(env))
+	cfg := config.GetConfig()
 	db := config.ConnectDB(cfg)
 	defer config.DisconnectDB(cfg, db.Client())
 
