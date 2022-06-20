393c69705cd2d06a13e423da444e0df7a381ac3d
Alex <alexandre@yakow.io>
2022-06-20 20:58:48 +0200

    feat: (service) amadeus get access token

--- server/service/amadeus_service.go
+++ server/service/amadeus_service.go
@@ -1,11 +1,16 @@
 package service
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/alexandre-pinon/epic-road-trip/config"
 	"github.com/alexandre-pinon/epic-road-trip/model"
+	"github.com/gin-gonic/gin"
 )
 
 type amadeusService struct {
@@ -21,8 +26,33 @@ func NewAmadeusService(cfg config.Config) AmadeusService {
 }
 
 func (svc *amadeusService) GetAccessToken(amadeusBaseUrl string) (string, error) {
-	return "", &model.AppError{
-		StatusCode: http.StatusNotImplemented,
-		Err:        errors.New("TODO: implement GetAccessToken"),
+	requestBody := url.Values{}
+	requestBody.Set("grant_type", "client_credentials")
+	requestBody.Set("client_id", svc.cfg.AmadeusKey)
+	requestBody.Set("client_secret", svc.cfg.AmadeusSecret)
+
+	response, err := http.Post(
+		fmt.Sprintf("%s/v1/security/oauth2/token", amadeusBaseUrl),
+		gin.MIMEPOSTForm,
+		strings.NewReader(requestBody.Encode()),
+	)
+	if err != nil {
+		return "", &model.AppError{
+			StatusCode: response.StatusCode,
+			Err:        err,
+		}
 	}
+
+	if response.StatusCode == http.StatusUnauthorized {
+		return "", &model.AppError{
+			StatusCode: response.StatusCode,
+			Err:        errors.New("invalid client credentials"),
+		}
+	}
+
+	defer response.Body.Close()
+	responseBody := model.AccessTokenResponse{}
+	json.NewDecoder(response.Body).Decode(&responseBody)
+
+	return responseBody.AccessToken, nil
 }
