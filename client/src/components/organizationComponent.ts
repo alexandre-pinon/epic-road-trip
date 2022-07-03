f5e3f4a7c8089dbab2232b1669a570a7e3a296c7
Keisay <kinsay.soundcloud@gmail.com>
2022-07-03 16:04:31 +0200

    feat: adding enjoy/sleep in organization components

--- client/src/components/organizationComponent.ts
+++ client/src/components/organizationComponent.ts
@@ -13,12 +13,14 @@ export { AppHeader } from "./AppHeader/Header"
 export { Home } from "./Home/Home"
 
 // Trip components
-export {StartEndTrip} from "./Trip/startEndTrip"
+export { StartEndTrip } from "./Trip/startEndTrip"
 export { Travel } from "./Trip/Travel"
 export { City } from "./Trip/City"
 export { ResumeTrip } from "./Trip/resumeTrip"
-export {Test} from "./Trip/test"
+// export {Test} from "./Trip/test"
 
 // Mock-Service-Worker components
 export { MockTest } from "./MockTest/MockTest"
 
+export { Enjoy } from "./Enjoy/Enjoy"
+export { Sleep } from "./Sleep/Sleep" 
