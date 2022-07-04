211d96ff883ffcf7915379c4495c0745d54427f5
Alex <alexandre@yakow.io>
2022-07-04 07:07:36 +0200

    setup: create structs

--- server/model/roadtrip.go
+++ server/model/roadtrip.go
@@ -14,3 +14,39 @@ type RoadTrip struct {
 	TripSteps   []primitive.ObjectID `json:"tripSteps"`
 	Itineraries []primitive.ObjectID `json:"itineraries"`
 }
+
+type TripStep struct {
+	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	City      string             `json:"city"`
+	Startdate time.Time          `json:"startdate"`
+	Enddate   time.Time          `json:"enddate"`
+	Travel    Itinerary          `json:"travel"`
+	Enjoy     Enjoy              `json:"enjoy"`
+	Sleep     Sleep              `json:"sleep"`
+	Eat       Eat                `json:"eat"`
+	Drink     Drink              `json:"drink"`
+}
+type Enjoy struct {
+	ID       int     `json:"id"`
+	Name     string  `json:"name"`
+	Rating   float64 `json:"rating"`
+	Vicinity string  `json:"vicinity"`
+}
+type Sleep struct {
+	ID       int     `json:"id"`
+	Name     string  `json:"name"`
+	Rating   float64 `json:"rating"`
+	Vicinity string  `json:"vicinity"`
+}
+type Eat struct {
+	ID       int     `json:"id"`
+	Name     string  `json:"name"`
+	Rating   float64 `json:"rating"`
+	Vicinity string  `json:"vicinity"`
+}
+type Drink struct {
+	ID       int     `json:"id"`
+	Name     string  `json:"name"`
+	Rating   float64 `json:"rating"`
+	Vicinity string  `json:"vicinity"`
+}
