9c28a004ae4c922061df955204fc183bf88d939d
Keisay <kinsay.soundcloud@gmail.com>
2022-07-07 11:45:10 +0200

    design: improving resume trip

--- client/src/components/Trip/resumeTrip.tsx
+++ client/src/components/Trip/resumeTrip.tsx
@@ -1,18 +1,71 @@
 import { useNavigate } from 'react-router-dom';
-import { Avatar, Button, Center, Title, Card, Image, Text, Paper, Space, Grid } from '@mantine/core';
+import { Avatar, Button, Center, Title, Card, Image, Text, Paper, Space, Grid, createStyles, Container, SimpleGrid, Divider } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import React, { useState } from "react";
 import axios from "axios";
 
+
+const useStyles = createStyles((theme) => ({
+    wrapper: {
+        paddingTop: 80,
+        paddingBottom: 50,
+    },
+
+    item: {
+        display: 'flex',
+    },
+
+    itemIcon: {
+        padding: theme.spacing.xs,
+        marginRight: theme.spacing.md,
+    },
+
+    itemTitle: {
+        marginBottom: theme.spacing.xs / 2,
+    },
+
+    supTitle: {
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        fontWeight: 800,
+        fontSize: theme.fontSizes.sm,
+        color: theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 4 : 8],
+        letterSpacing: 0.5,
+    },
+
+    title: {
+        lineHeight: 1,
+        textAlign: 'center',
+        marginTop: theme.spacing.xl,
+    },
+
+    description: {
+        textAlign: 'center',
+        marginTop: theme.spacing.xs,
+    },
+
+    highlight: {
+        backgroundColor:
+            theme.colorScheme === 'dark'
+                ? theme.fn.rgba(theme.colors[theme.primaryColor][6], 0.55)
+                : theme.colors[theme.primaryColor][0],
+        padding: 5,
+        paddingTop: 0,
+        borderRadius: theme.radius.sm,
+        display: 'inline-block',
+        color: theme.colorScheme === 'dark' ? theme.white : 'inherit',
+    },
+}));
 
 
 
 
 export function ResumeTrip({ fulTrip }: any) {
     const navigate = useNavigate();
+    const { classes } = useStyles();
 
-    const confirmTripPost = () => {
-        let params = [
+    const [subTrip, setSubTrip] = useState(
+        [
             {
                 city: fulTrip.startCity,
                 startdate: fulTrip.startDateValue,
@@ -126,66 +179,130 @@ export function ResumeTrip({ fulTrip }: any) {
 
     return (
         <>
-            <Center>
-                <Title
-                    align="center"
-                    sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 100 })}
-                >
-                    This is your resume trip 🚌
+            <Container size={700} className={classes.wrapper}>
+                <Center>
+                    <Title
+                        align="center"
+                        sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 100 })}
+                    >
+                        This is your resume trip 🚌
 
-                </Title>
+                    </Title>
+                    <Space h="xl" />
+                </Center>
+
                 <Space h="xl" />
-            </Center>
 
-            <Space h="xl" />
+                <Paper shadow="xs" radius="lg" p="lg" withBorder>
+                    <Center><Title
+                        order={2}
+                        align="center"
+                        sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 100 })}
+                    >
+                        Your trip
 
-            <Paper shadow="xs" radius="lg" p="lg" withBorder>
-                <Center><Title
-                    order={2}
-                    align="center"
-                    sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 100 })}
-                >
-                    Your trip
 
-                </Title></Center>
-                <h3>Cities</h3>
-                <Grid><Text weight={700}>Start City :  </Text><Text> &nbsp;{fulTrip.startCity}</Text></Grid>
-                <Grid><Text weight={700}>End City :  </Text><Text> &nbsp;{fulTrip.endCity}</Text></Grid>
-                <Space h="md" />
-                <h3>Dates</h3>
-                <Grid><Text weight={700}>Start Date :  </Text><Text> &nbsp;{fulTrip.startDateValue.toDateString()}</Text></Grid>
-                <Grid><Text weight={700}>End Date :  </Text><Text> &nbsp;{fulTrip.endDateValue.toDateString()}</Text></Grid>
-                <Space h="md" />
-                <h3>Itinerary</h3>
-                <Grid><Text weight={700}>Departure city :  </Text><Text> &nbsp;{fulTrip.selectedTravel.cityDeparture}</Text></Grid>
-                <Grid><Text weight={700}>Arrival city :  </Text><Text> &nbsp;{fulTrip.selectedTravel.cityArrival}</Text></Grid>
-                <Grid><Text weight={700}>Departure time :  </Text><Text> &nbsp;{fulTrip.selectedTravel.startDate}</Text></Grid>
-                <Grid><Text weight={700}>Arrival time :  </Text><Text> &nbsp;{fulTrip.selectedTravel.endDate}</Text></Grid>
-                <Grid><Text weight={700}>Duration :  </Text><Text> &nbsp;{fulTrip.selectedTravel.duration}</Text></Grid>
-                <Space h="md" />
-                <h3>Enjoy Activities</h3>
-                <Grid><Text weight={700}>Name :  </Text><Text> &nbsp;{fulTrip.enjoyName.name}</Text></Grid>
-                <Grid><Text weight={700}>Vicinity :  </Text><Text> &nbsp;{fulTrip.enjoyName.vicinity}</Text></Grid>
-                <Grid><Text weight={700}>Rating :  </Text><Text> &nbsp;{fulTrip.enjoyName.rating}</Text></Grid>
-                <Space h="md" />
-                <h3>Eat</h3>
-                <Grid><Text weight={700}>Restaurant name :  </Text><Text> &nbsp;{fulTrip.eat.name}</Text></Grid>
-                <Grid><Text weight={700}>Address :  </Text><Text> &nbsp;{fulTrip.eat.vicinity}</Text></Grid>
-                <Grid><Text weight={700}>Rating :  </Text><Text> &nbsp;{fulTrip.eat.rating}</Text></Grid>
-                <Space h="md" />
-                <h3>Drink</h3>
-                <Grid><Text weight={700}>Restaurant name :  </Text><Text> &nbsp;{fulTrip.drink.name}</Text></Grid>
-                <Grid><Text weight={700}>Address :  </Text><Text> &nbsp;{fulTrip.drink.vicinity}</Text></Grid>
-                <Grid><Text weight={700}>Rating :  </Text><Text> &nbsp;{fulTrip.drink.rating}</Text></Grid>
-                <Space h="md" />
-                <h3>Hotel</h3>
-                <Grid><Text weight={700}>Hotel :  </Text><Text> &nbsp;{fulTrip.sleep.nameSleep}</Text></Grid>
-                <Grid><Text weight={700}>Address :  </Text><Text> &nbsp;{fulTrip.sleep.vicinitySleep}</Text></Grid>
-                <Grid><Text weight={700}>Rating :  </Text><Text> &nbsp;{fulTrip.sleep.ratingSleep}</Text></Grid>
-                <Center><Button onClick={confirmTripPost}>Confirm your trip !</Button></Center>
-            </Paper>
+                    </Title></Center>
+                    <Title order={5}>🌇</Title>
+                    <Space h="xs" />
+                    <Grid><Text weight={300}>Start City :  </Text><Text> &nbsp;{fulTrip.startCity}</Text></Grid>
+                    <Grid><Text weight={300}>End City :  </Text><Text> &nbsp;{fulTrip.endCity}</Text></Grid>
+                    <Space h="md" />
+                    <Title order={5}>📅</Title>
+                    <Space h="xs" />
+                    <Grid><Text weight={300}>Start Date :  </Text><Text weight={400}> &nbsp;{fulTrip.startDateValue.toDateString()}</Text></Grid>
+                    <Grid><Text weight={300}>End Date :  </Text><Text weight={400}> &nbsp;{fulTrip.endDateValue.toDateString()}</Text></Grid>
+                    <Space h="md" />
+                    <Title order={5}>✈️</Title>
+                    <Space h="xs" />
+                    <Grid><Text weight={300}>Departure city :  </Text><Text weight={400}> &nbsp;{fulTrip.selectedTravel.cityDeparture}</Text></Grid>
+                    <Grid><Text weight={300}>Arrival city :  </Text><Text weight={400}> &nbsp;{fulTrip.selectedTravel.cityArrival}</Text></Grid>
+                    <Grid><Text weight={300}>Departure time :  </Text><Text weight={400}> &nbsp;{fulTrip.selectedTravel.startDate}</Text></Grid>
+                    <Grid><Text weight={300}>Arrival time :  </Text><Text weight={400}> &nbsp;{fulTrip.selectedTravel.endDate}</Text></Grid>
+                    <Grid><Text weight={300}>Duration :  </Text><Text weight={400}> &nbsp;{fulTrip.selectedTravel.duration}</Text></Grid>
+                    <Space h="xl" />
+                    <Divider my="sm" />
+                    <Center><Title
+                        order={2}
+                        align="center"
+                        sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 100 })}
+                    >
+                        {fulTrip.startCity}
 
-            {/*
+                    </Title></Center>
+                    <Space h="xl" />
+
+
+                    <Title order={5}>🙂</Title>
+                    <Space h="xs" />
+                    <Grid><Text weight={300}>Name :  </Text><Text weight={400}> &nbsp;{fulTrip.enjoyName.name}</Text></Grid>
+                    <Grid><Text weight={300}>Vicinity :  </Text><Text weight={400}> &nbsp;{fulTrip.enjoyName.vicinity}</Text></Grid>
+                    <Grid><Text weight={300}>Rating :  </Text><Text weight={400}> &nbsp;{fulTrip.enjoyName.rating}</Text></Grid>
+                    <Space h="md" />
+                    <Title order={5}>🍔</Title>
+                    <Space h="xs" />
+                    <Grid><Text weight={300}>Restaurant name :  </Text><Text weight={400}> &nbsp;{fulTrip.eat.name}</Text></Grid>
+                    <Grid><Text weight={300}>Address :  </Text><Text weight={400}> &nbsp;{fulTrip.eat.vicinity}</Text></Grid>
+                    <Grid><Text weight={300}>Rating :  </Text><Text weight={400}> &nbsp;{fulTrip.eat.rating}</Text></Grid>
+                    <Space h="md" />
+                    <Title order={5}>🍺</Title>
+                    <Space h="xs" />
+                    <Grid><Text weight={300}>Restaurant name :  </Text><Text weight={400}> &nbsp;{fulTrip.drink.name}</Text></Grid>
+                    <Grid><Text weight={300}>Address :  </Text><Text weight={400}> &nbsp;{fulTrip.drink.vicinity}</Text></Grid>
+                    <Grid><Text weight={300}>Rating :  </Text><Text weight={400}> &nbsp;{fulTrip.drink.rating}</Text></Grid>
+                    <Space h="md" />
+                    <Title order={5}>😴</Title>
+                    <Space h="xs" />
+                    <Grid><Text weight={300}>Hotel :  </Text><Text weight={400}> &nbsp;{fulTrip.sleep.nameSleep}</Text></Grid>
+                    <Grid><Text weight={300}>Address :  </Text><Text weight={400}> &nbsp;{fulTrip.sleep.vicinitySleep}</Text></Grid>
+                    <Grid><Text weight={300}>Rating :  </Text><Text weight={400}> &nbsp;{fulTrip.sleep.ratingSleep}</Text></Grid>
+
+
+
+
+                    <Space h="xl" />
+                    <Divider my="sm" />
+                    <Center><Title
+                        order={2}
+                        align="center"
+                        sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 100 })}
+                    >
+                        {fulTrip.endCity}
+
+                    </Title></Center>
+                    <Space h="xl" />
+
+
+                    {/* Arrival City */}
+                    <Title order={5}>🙂</Title>
+                    <Space h="xs" />
+
+                    <Grid><Text weight={300}>Name :  </Text><Text weight={400}> &nbsp;{fulTrip.enjoyArrival.name}</Text></Grid>
+                    <Grid><Text weight={300}>Vicinity :  </Text><Text weight={400}> &nbsp;{fulTrip.enjoyArrival.vicinity}</Text></Grid>
+                    <Grid><Text weight={300}>Rating :  </Text><Text weight={400}> &nbsp;{fulTrip.enjoyArrival.rating}</Text></Grid>
+                    <Space h="md" />
+                    <Title order={5}>🍔</Title>
+                    <Space h="xs" />
+
+                    <Grid><Text weight={300}>Restaurant name :  </Text><Text weight={400}> &nbsp;{fulTrip.eatArrival.name}</Text></Grid>
+                    <Grid><Text weight={300}>Address :  </Text><Text weight={400}> &nbsp;{fulTrip.eatArrival.vicinity}</Text></Grid>
+                    <Grid><Text weight={300}>Rating :  </Text><Text weight={400}> &nbsp;{fulTrip.eatArrival.rating}</Text></Grid>
+                    <Space h="md" />
+                    <Title order={5}>🍺</Title>
+                    <Space h="xs" />
+
+                    <Grid><Text weight={300}>Restaurant name :  </Text><Text weight={400}> &nbsp;{fulTrip.drinkArrival.name}</Text></Grid>
+                    <Grid><Text weight={300}>Address :  </Text><Text weight={400}> &nbsp;{fulTrip.drinkArrival.vicinity}</Text></Grid>
+                    <Grid><Text weight={300}>Rating :  </Text><Text weight={400}> &nbsp;{fulTrip.drinkArrival.rating}</Text></Grid>
+                    <Space h="md" />
+                    <Title order={5}>😴</Title>
+                    <Space h="xs" />
+                    <Grid><Text weight={300}>Hotel :  </Text><Text weight={400}> &nbsp;{fulTrip.sleepArrival.nameSleep}</Text></Grid>
+                    <Grid><Text weight={300}>Address :  </Text><Text weight={400}> &nbsp;{fulTrip.sleepArrival.vicinitySleep}</Text></Grid>
+                    <Grid><Text weight={300}>Rating :  </Text><Text weight={400}> &nbsp;{fulTrip.sleepArrival.ratingSleep}</Text></Grid>
+                </Paper>
+
+                {/*
             <Paper shadow="xs" radius="lg" p="lg" withBorder>
                 {subTrip.map((subTrip, index) => (
                     <>
@@ -216,22 +333,24 @@ export function ResumeTrip({ fulTrip }: any) {
             */}
 
 
-            <Space h="xl" />
+                <Space h="xl" />
 
-            <Center>
-                <Button onClick={goToHome} variant="light" radius="xl">
-                    Go to Home Page
-                </Button>
-                <Button onClick={goToArrival} variant="light" radius="xl">
-                    {fulTrip.endCity}
-                </Button>
-                {/*
+                <Center>
+                    <Button onClick={goToHome} variant="light" radius="xl">
+                        Go to Home Page
+                    </Button>
+                    <Button onClick={goToArrival} variant="light" radius="xl">
+                        {fulTrip.endCity}
+                    </Button>
+                    {/*
                 <Button onClick={addSubTrip} variant="light" radius="xl">
                     Add an other sub-Trip
                 </Button>
                 */}
 
-            </Center>
+                </Center>
+            </Container>
+
         </>
     )
 }
