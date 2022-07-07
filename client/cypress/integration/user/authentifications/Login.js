f92bf182b0ef8af50c954797e2790ad31121fc56
TharickABDUL <abdultharick@gmail.com>
2022-07-07 16:48:36 +0200

    test: login page finish

--- client/cypress/integration/user/authentifications/Login.js
+++ client/cypress/integration/user/authentifications/Login.js
@@ -12,4 +12,58 @@ describe('Login page', () => {
     })
 
 
+
+    it('Check attributes Login page', () => {
+        cy.visit('http://localhost:3000/login')
+        //Input
+        cy.get('[data-testid="input"]')
+            .should('have.attr', 'required')
+
+        cy.get('[data-testid="input"]')
+            .should('have.attr', 'placeholder')
+            .and('equal', 'Your email')
+
+        //Password
+        cy.get('[data-testid="password"]')
+            .should('have.attr', 'required')
+
+        cy.get('[data-testid="password"]')
+            .should('have.attr', 'placeholder')
+            .and('equal', 'Your password')
+    })
+
+
+
+    it('check Register button Page from Login page', () => {
+        cy.visit('http://localhost:3000/login')
+
+        cy.get('[data-testid="registerPage"] > .mantine-Text-root').click()
+        cy.wait(500)
+        cy.location('href').should('include', '/register#')
+    })
+
+
+
+    it('check input Login page', () => {
+        cy.visit('http://localhost:3000/login')
+
+        cy.get('[data-testid="input"]').type('tharick@gmail.com')
+        cy.get('[data-testid="input"]').should('have.value', 'tharick@gmail.com')
+
+        cy.get('[data-testid="password"]').type('12345678')
+        cy.get('[data-testid="password"]').should('have.value', '12345678')
+    })
+
+
+
+    it('check Login Valid', () => {
+        cy.visit('http://localhost:3000/login')
+
+        cy.get('[data-testid="input"]').type('tharick@gmail.com')
+        cy.get('[data-testid="password"]').type('12345678')
+        cy.wait(500)
+        cy.get('[data-testid="signup"]').click()
+        cy.location('href').should('include', '/')
+
+    })
 })
